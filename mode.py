012dcedc5e787bf44b664265d13a4eb489961462
SaiSravya6 <saisravyagarapati6@gmail.com>
2025-12-17 11:19:50 +0530

    Mode Calculation Using Python
    Mode Calculation Using Python –
    
    In this program, I calculated the mode of a given set of numbers using Python.
    
    First, I accepted multiple numeric values from the user using the input() function and converted them into a list of numbers.
    
    Next, I created an empty dictionary to store the frequency of each number. I iterated through the list and counted how many times each value appeared by updating the dictionary.
    
    Then, I found the maximum frequency among all the values. The number or numbers that occurred this maximum number of times were identified as the mode.
    
    If only one value had the highest frequency, it was displayed as the mode. If more than one value had the same highest frequency, all such values were displayed as modes.
    
    Finally, I printed the result using the print() statement.
    
    Thus, the program successfully calculates the mode of the given data using Python.

---
+++ mode.py
@@ -0,0 +1,16 @@
+# Mode Calculation
+
+numbers = list(map(float, input("Enter numbers separated by space: ").split()))
+
+freq = {}
+for num in numbers:
+    freq[num] = freq.get(num, 0) + 1
+
+max_freq = max(freq.values())
+
+modes = [key for key, value in freq.items() if value == max_freq]
+
+if len(modes) == 1:
+    print("Mode =", modes[0])
+else:
+    print("Modes =", modes)
